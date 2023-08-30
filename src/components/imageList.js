2284738a302084438a0dff9124b9aacc57d6036d
Nick <nikitalytvynov0506@gmail.com>
2023-08-30 14:47:32 +0300

    si

--- src/components/imageList.js
+++ src/components/imageList.js
@@ -1,5 +1,12 @@
-function imageList() {
-  return <div>imageList</div>;
+import './ImageList.css'
+import ImageShow from "./imageShow";
+
+function imageList({ images }) {
+    const renderedImages = images.map((image, index) => {
+        return <ImageShow key={image.id} image={image} />
+    }) 
+    
+  return <div className="image-list">{renderedImages}</div>;
 }
 
 export default imageList;
